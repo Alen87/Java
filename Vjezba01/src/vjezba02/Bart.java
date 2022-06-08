aa05cf07e50480391024910b9beb8824cdfe58de
Alen Boric <boricalen@live.com>
2022-06-08 12:05:05 +0200

    vjezba02

---
+++ Vjezba01/src/vjezba02/Bart.java
@@ -0,0 +1,24 @@
+package vjezba02;
+
+public class Bart {
+
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("     |\\/\\  ,.\r\n"
+		   	+ "    /   `' |,-,\r\n"
+			+ "   /         /_\r\n"
+			+ " _/            /\r\n"
+			+ "(.-,--.       /\r\n"
+			+ "/o/  o \\     /\r\n"
+			+ "\\_\\    /   _/\r\n"
+			+ "(__`--'    _)\r\n"
+			+ " /         |\r\n"
+			+ "(_____,'    \\  hjw\r\n"
+			+ "   \\_       _\\\r\n"
+			+ "     `._..-'");
+
+	}
+
+}
