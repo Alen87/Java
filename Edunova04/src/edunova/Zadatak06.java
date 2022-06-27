dca2de9e2d111d0209bb2f9b3dce29bbc57ba407
Alen Boric <boricalen@live.com>
2022-06-27 23:49:48 +0200

    Create Zadatak06.java

---
+++ Edunova04/src/edunova/Zadatak06.java
@@ -0,0 +1,22 @@
+package edunova;
+
+//Za dani jednodimenzinalni niz
+// cjelih brojeva program ispisuje 
+// najveæi
+
+public class Zadatak06 {
+	public static void main(String[] args) {
+
+		int[] niz = { 5, 5, 8, 6, -6, 8, -1, 0, 9,35, 0 };
+
+		int max = Integer.MIN_VALUE;
+		for (int a : niz) {
+			if (a > max) {
+				max = a;
+			}
+
+		}
+
+		System.out.println(max);
+	}
+}
