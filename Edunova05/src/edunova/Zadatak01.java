253ed6dada1f9c948c5e47c1201ae9accad4f284
Alen Boric <boricalen@live.com>
2022-06-28 00:42:19 +0200

    Zadatak01

---
+++ Edunova05/src/edunova/Zadatak01.java
@@ -0,0 +1,28 @@
+package edunova;
+
+//Deklarirajte niz decimalnih
+	// brojeva dužine 12 i svim elementima
+	// niza postavite vrijednost 3,14
+	// s while petljom
+	
+
+public class Zadatak01 {
+	public static void main(String[] args) {
+		
+		
+		double[]niz=new double[12];
+		
+		int i=0;
+		
+		while(i<niz.length) {
+		niz[i]=3.14;		
+		System.out.println(niz[i]); 
+	    ++i;
+		}
+		
+		
+		
+		
+	}
+
+}
