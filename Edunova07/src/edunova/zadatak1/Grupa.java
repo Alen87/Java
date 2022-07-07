efce2aec8a9962ca8617c4d62a8558953fb7a3c2
Alen Boric <boricalen@live.com>
2022-07-07 20:23:47 +0200

    Edunova07

---
+++ Edunova07/src/edunova/zadatak1/Grupa.java
@@ -0,0 +1,14 @@
+package edunova.zadatak1;
+
+import java.util.Date;
+
+public class Grupa {
+	
+	private String naziv;
+	private Date datumPocetka;
+	private Smjer smjer;
+	private Predavac predavac;
+	
+	
+
+}
---
+++ Edunova07/src/edunova/zadatak1/Predavac.java
@@ -0,0 +1,8 @@
+package edunova.zadatak1;
+
+public class Predavac {
+	
+	private String ime;
+	private String prezime;
+
+}
