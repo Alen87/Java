ec551a296f1fff99c8a890f7d3e86b0f8dda9c3b
Alen Boric <boricalen@live.com>
2022-07-07 19:25:46 +0200

    Edunova07

--- Edunova07/src/edunova/Profesor.java
+++ Edunova07/src/edunova/Profesor.java
@@ -5,5 +5,34 @@ public class Profesor {
 	private String ime;
 	private String prezime;
 	private Mjesto mjesto;
+	
+	
+	
+	
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	public String getPrezime() {
+		return prezime;
+	}
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+	public Mjesto getMjesto() {
+		return mjesto;
+	}
+	public void setMjesto(Mjesto mjesto) {
+		this.mjesto = mjesto;
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
