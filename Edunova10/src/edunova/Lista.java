e3dec63909471c98dc1cd138816bc4a91f535fbc
Alen Boric <boricalen@live.com>
2022-07-12 22:52:22 +0200

    Edunova10

---
+++ Edunova10/src/edunova/Lista.java
@@ -0,0 +1,44 @@
+package edunova;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edunova.model.Predavac;
+
+public class Lista {
+	
+	public Lista() {
+	
+		// sintaksa do jave 1.5
+				// neæemo ju kristiti
+				/*
+				List lista = new ArrayList();
+				lista.add("Osijek");
+				lista.add(new Predavac());
+				*/
+				
+				// s lijeve strane znaka jednakosti
+				// suèelje a s desne implementacijska klasa
+		
+		List <Predavac>lista = new ArrayList<>();
+		Predavac p=new Predavac();
+		p.setIme("Pero");
+		lista.add(p);
+		
+		System.out.println(lista.get(0).getIme());
+		
+		
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		new Lista();
+		
+		
+		
+	}
+	
+}
