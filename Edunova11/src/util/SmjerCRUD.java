07fab0fe4437d79fdd0992ceee05298798fe2446
Alen Boric <boricalen@live.com>
2022-08-24 23:19:49 +0200

    SmjerCRUD

---
+++ Edunova11/src/util/SmjerCRUD.java
@@ -0,0 +1,44 @@
+package util;
+
+import java.util.List;
+
+import model.Smjer;
+
+public class SmjerCRUD {
+	
+	public static Smjer unosNovog() {
+		
+		Smjer s= new Smjer();
+		s.setSifra(Pomocno.ucitajInt("Unesite sifru smjera(pozitivan broj)", true));
+		s.setNaziv(Pomocno.ucitajString("Unesi naziv smjera"));
+		// ostalo DZ
+		return s;
+		
+	}
+	
+	
+	public static void ispis(List<Smjer>smjerovi) {
+		int rb=1;
+		
+		System.out.println("");
+		System.out.println("Smjerovi u aplikaciji");
+		for(Smjer s: smjerovi) {
+			System.out.println(rb++ + ". " + s.getNaziv());
+		}
+		System.out.println("-------------------");
+	}
+	
+	public static void promjena(Smjer s) {
+		
+		s.setSifra(Pomocno.ucitajInt("sifra (" + s.getSifra() + ")", true));
+		s.setNaziv(Pomocno.ucitajString("naziv (" + s.getNaziv() + ")"));
+
+		// ostalo DZ
+		
+	}
+	
+	
+	
+	
+
+}
