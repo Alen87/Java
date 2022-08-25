3d6dde59b822b03ccec77b8439c9da49fa4d554d
Alen Boric <boricalen@live.com>
2022-08-26 00:25:06 +0200

    Konzolna

--- Edunova11/src/edunova/Start.java
+++ Edunova11/src/edunova/Start.java
@@ -8,101 +8,152 @@ import model.Grupa;
 import model.Polaznik;
 import model.Predavac;
 import model.Smjer;
+import util.GrupaCRUD;
 import util.Pomocno;
-
-
+import util.SmjerCRUD;
 
 public class Start {
-	
-	private List<Smjer>smjerovi;
-	private List<Polaznik>polaznici;
-	private List<Predavac>predavaci;
+
+	private List<Smjer> smjerovi;
+	private List<Polaznik> polaznici;
+	private List<Predavac> predavaci;
 	private List<Grupa> grupe;
 
 	public Start() {
-		smjerovi = new ArrayList();
-		polaznici = new ArrayList();
-		predavaci = new ArrayList();
-		grupe = new ArrayList();
+		smjerovi = new ArrayList<>();
+		polaznici = new ArrayList<>();
+		predavaci = new ArrayList<>();
+		grupe = new ArrayList<>();
 		inicijalniPodaci();
-		Pomocno.ulaz = 
-				new Scanner(System.in);
+		Pomocno.ulaz = new Scanner(System.in);
 		System.out.println("***** Edunova konzolna aplikacija V1 *****");
 		izbornik();
 		Pomocno.ulaz.close();
 	}
-	public void inicijalniPodaci() {
+
+	private void inicijalniPodaci() {
 		Smjer s = new Smjer();
 		s.setSifra(1);
 		s.setNaziv("Java");
 		smjerovi.add(s);
-		s=new Smjer();
+		s = new Smjer();
 		s.setSifra(2);
 		s.setNaziv("PHP");
 		smjerovi.add(s);
-		
-		
-		polaznici.add(new Polaznik(1,"Ana","Zimska","","",""));
-		polaznici.add(new Polaznik(2,"Marko","Tot","","",""));
-		polaznici.add(new Polaznik(3,"Marija","Sven","","",""));
-		
-		
+
+		polaznici.add(new Polaznik(1, "Ana", "Zimska", "", "", ""));
+		polaznici.add(new Polaznik(2, "Marko", "Tot", "", "", ""));
+		polaznici.add(new Polaznik(3, "Marija", "Sven", "", "", ""));
+
 		Predavac p = new Predavac();
 		p.setSifra(1);
 		p.setIme("Karlo");
-		p.setPrezime("Cec");
+		p.setPrezime("Èeè");
 		predavaci.add(p);
-		
-		
-		
 	}
-	
+
 	private void pokreniProgram() {
-		switch (Pomocno.ucitajInt(
-				"Odaberi program", 1, 5)) {
-		case 1: 
+		switch (Pomocno.ucitajInt("Odaberi program", 1, 5)) {
+		case 1:
 			smjerovi();
 			break;
-		case 2: 
+		case 2:
 			grupe();
 			break;
-		case 3: 
+		case 3:
 			polaznici();
 			break;
-		case 4: 
+		case 4:
 			predavaci();
 			break;
 		case 5:
 			System.out.println("Doviðenja");
 		}
 	}
-	
+
 	private void predavaci() {
-		System.out.println("domaca zadaca");
-		//za sada
+		System.out.println("Domaæa zadaæa");
+		// za sada
 		izbornik();
 	}
-	
+
 	private void polaznici() {
-		System.out.println("domaca zadaca");
-		//za sada
+		System.out.println("Domaæa zadaæa");
+		// za sada
 		izbornik();
 	}
 
 	private void grupe() {
 		System.out.println("Program grupe");
-		//za sada
-		izbornik();
+		izbornikProgram();
+		pokreniGrupaAkcija();
+	}
+
+	private void pokreniGrupaAkcija() {
+		switch (Pomocno.ucitajInt("Odaberite akciju", 1, 5)) {
+		case 1:
+			grupe.add(GrupaCRUD.unosNove(smjerovi, predavaci, polaznici));
+			grupe();
+			break;
+		case 2:
+			GrupaCRUD.ispis(grupe);
+			grupe();
+			break;
+		case 3:
+
+			grupe();
+			break;
+		case 4:
+
+			grupe();
+			break;
+		case 5:
+			izbornik();
+		}
 	}
 
 	private void smjerovi() {
-		System.out.println("Doðe izbornik za smjerove");
-		//za sada
-		izbornik();
+		System.out.println("Program smjerovi");
+		izbornikProgram();
+		pokreniSmjerAkciju();
+	}
+
+	private void izbornikProgram() {
+		System.out.println("1. Unos");
+		System.out.println("2. Pregled");
+		System.out.println("3. Promjena");
+		System.out.println("4. Brisanje");
+		System.out.println("5. povratak na glavni izbornik");
+	}
+
+	private void pokreniSmjerAkciju() {
+		switch (Pomocno.ucitajInt("Odaberite akciju", 1, 5)) {
+		case 1:
+			smjerovi.add(SmjerCRUD.unosNovog());
+			smjerovi();
+			break;
+		case 2:
+			SmjerCRUD.ispis(smjerovi);
+			smjerovi();
+			break;
+		case 3:
+			SmjerCRUD.ispis(smjerovi);
+			SmjerCRUD.promjena(smjerovi.get(Pomocno.ucitajInt("Odaberi smjer", 1, smjerovi.size()) - 1));
+			smjerovi();
+			break;
+		case 4:
+			SmjerCRUD.ispis(smjerovi);
+			smjerovi.remove(Pomocno.ucitajInt("Odaberi smjer", 1, smjerovi.size()) - 1);
+			smjerovi();
+			break;
+		case 5:
+			izbornik();
+		}
+
 	}
 
 	private void izbornik() {
-		
+
 		System.out.println("");
 		System.out.println("Glavni izbornik");
 		System.out.println("1. Smjerovi");
@@ -111,12 +162,12 @@ public class Start {
 		System.out.println("4. Predavaèi");
 		System.out.println("5. Izlaz");
 		System.out.println("");
-		pokreniProgram(); 
-		
+		pokreniProgram();
+
 	}
 
 	public static void main(String[] args) {
 		new Start();
 	}
-	
+
 }
