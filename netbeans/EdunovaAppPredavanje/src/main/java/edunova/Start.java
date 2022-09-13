1e210d1013ef58ea470a787faca9382004c6f057
Alen Boric <boricalen@live.com>
2022-09-13 21:42:44 +0200

    EdunovaApp

--- netbeans/EdunovaAppPredavanje/src/main/java/edunova/Start.java
+++ netbeans/EdunovaAppPredavanje/src/main/java/edunova/Start.java
@@ -5,9 +5,14 @@
 
 package edunova;
 
+import edunova.controller.ObradaSmjer;
+import edunova.model.Smjer;
+import edunova.util.EdunovaException;
 import edunova.util.HibernateUtil;
 import edunova.util.PocetniInsert;
 import java.math.BigDecimal;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,7 +22,31 @@ public class Start {
 
     public Start() {
        
-        PocetniInsert.izvedi();
+//        PocetniInsert.izvedi();
+//        for(Smjer s : new ObradaSmjer().read()){
+//            System.out.println(s.getNaziv());         
+//        }
+
+//        ObradaSmjer os = new ObradaSmjer();
+//        Smjer s = new Smjer();
+//        s.setNaziv("Prvi pomoću kontrolera");
+//        s.setCijena(BigDecimal.ONE);
+//        os.setEntitet(s);
+//        try {
+//            os.create();
+//        } catch (EdunovaException ex) {
+//            System.out.println(ex.getPoruka());
+//        }
+//        s = new Smjer();
+//        s.setNaziv("Prvi pomoću kontrolera");
+//        os.setEntitet(s);
+//        try {
+//            os.create();
+//        } catch (EdunovaException ex) {
+//            System.out.println(ex.getPoruka());
+//        }
+        
+        new PocetniInsert();
       
     }
     
